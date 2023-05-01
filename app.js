d1feb0c5f84801ee4584f47a65c0a11d58d9706d
Artem_WORK <art.puhov@gmail.com>
2023-05-01 20:04:30 +0300

    fix: app.js

--- app.js
+++ app.js
@@ -4,48 +4,56 @@ const keyboardLayout = [
     ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'],
     ['Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter'],
     ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'Shift'],
-    ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Ctrl']
-  ];
-  
-  // Создаем элемент клавиатуры
-  const keyboardElement = document.createElement('div');
-  keyboardElement.classList.add('keyboard');
-  
-  // Создаем элементы кнопок и добавляем их в клавиатуру
-  keyboardLayout.forEach(row => {
+    ['Ctrl', 'Win', 'Alt', 'space', 'Alt', 'Ctrl']
+];
+
+// Создаем элемент клавиатуры
+const keyboardElement = document.createElement('div');
+keyboardElement.classList.add('keyboard');
+
+// Создаем элементы кнопок и добавляем их в клавиатуру
+keyboardLayout.forEach(row => {
     const rowElement = document.createElement('div');
     rowElement.classList.add('keyboard-row');
-  
+
     row.forEach(key => {
-      const keyElement = document.createElement('div');
-      keyElement.classList.add('keyboard-key');
-      keyElement.textContent = key;
-  
-      // Добавляем обработчик события клика на кнопку клавиатуры
-      keyElement.addEventListener('click', () => {
-        // Добавляем символ в текстовое поле
-        textFieldElement.value += key;
-      });
-  
-      rowElement.appendChild(keyElement);
+        const keyElement = document.createElement('div');
+        keyElement.classList.add('keyboard-key');
+        keyElement.textContent = key;
+
+        // Добавляем обработчик события клика на кнопку клавиатуры
+        keyElement.addEventListener('click', () => {
+            // Добавляем символ в текстовое поле
+            textFieldElement.value += key;
+        });
+
+        rowElement.appendChild(keyElement);
     });
-  
+
     keyboardElement.appendChild(rowElement);
-  });
-  
-  // Создаем текстовое поле
-  const textFieldElement = document.createElement('textarea');
-  textFieldElement.classList.add('text-field');
-  
-  // Добавляем клавиатуру и текстовое поле на страницу
-  document.body.appendChild(keyboardElement);
-  document.body.appendChild(textFieldElement);
-  
-  // Выбираем все специальные клавиши и применяем стили
-  const specialKeys = document.querySelectorAll('.keyboard-key:not(:empty)');
-  specialKeys.forEach(key => {
-    if (key.textContent === 'Tab' || key.textContent === 'Caps Lock' || key.textContent === 'Enter' || key.textContent === 'Shift' || key.textContent === 'Backspace' || key.textContent === 'Delete' || key.textContent === 'Space') {
-      key.classList.add(key.textContent.toLowerCase() + '-key');
+});
+
+// Создаем текстовое поле
+const textFieldElement = document.createElement('textarea');
+textFieldElement.classList.add('text-field');
+
+// Добавляем клавиатуру и текстовое поле на страницу
+document.body.appendChild(keyboardElement);
+document.body.appendChild(textFieldElement);
+
+// Выбираем все специальные клавиши и применяем стили
+const specialKeys = document.querySelectorAll('.keyboard-key:not(:empty)');
+specialKeys.forEach(key => {
+    const keyText = key.textContent.toLowerCase();
+    if (['tab', 'caps lock', 'enter', 'shift', 'backspace', 'delete', 'space'].includes(keyText)) {
+        key.classList.add(keyText + '-key');
     }
-  });
-  
+});
+
+
+
+
+
+
+
+
